38efc08ed6da3e1c77154b707399fad408f1b5ef
Erika Vasquez <etvasquez@utpl.edu.ec>
2019-11-04 00:06:49 -0500

    Agregar metodo WINKLERDISTANCE

--- src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
@@ -1,21 +1,31 @@
 package ec.edu.utpl.sic.arqapl;
 
 
+import ec.edu.utpl.sic.arqapl.metrics.Cosine;
 import ec.edu.utpl.sic.arqapl.metrics.Jaccard;
+import ec.edu.utpl.sic.arqapl.metrics.JardWinkleDistance;
 import ec.edu.utpl.sic.arqapl.metrics.Similaritable;
+
+import java.util.ArrayList;
 
 public class TweetSimilarity {
     private Similaritable algorithm;
+    private ArrayList<Similaritable> Algorith;
 
     public TweetSimilarity() {
-        algorithm = new Jaccard();
+        Algorith.add(new Jaccard());
+        Algorith.add(new Cosine());
+        Algorith.add(new JardWinkleDistance());
     }
 
+
     public TweetSimilarity(Similaritable algorithm) {
+
         this.algorithm = algorithm;
     }
 
     public double similarity(String tweet1, String tweet2) {
+
         return algorithm.similarity(tweet1, tweet2);
     }
 }
